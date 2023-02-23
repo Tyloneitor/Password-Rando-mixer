708a27cc34dbdab38487833a16ecf277a0bd1258
tyloneitor <rodri329@go.stockton.edu>
2023-02-22 20:24:27 -0500

    confused lol

--- Develop/script.js
+++ Develop/script.js
@@ -3,9 +3,9 @@ var generateBtn = document.getElementById("generate");
 
 
 var length = 8-128;
-var charset = ["a","b","c"defghijklmnopqrstuvwxyz"]
-var charset = [ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789]
-var charset = []retVal = "";
+var charset = ["a","b","c","d","e","f","g","h","i","j","k","m","n","o","p","q","r","s","t","u","v","w","","x","y","z"]
+var charset = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","","X","Y","Z"]
+var charset = ["0","1","2","4","5","6","7","8","9"]retVal = "";
 for (var i = 0; <= length; i++)
 { var ranchar = math.random(length);
 writePassword += charset(randonNumber, randonNumber +1);
