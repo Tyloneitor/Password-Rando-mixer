11f5f9807ccce5bc32d2fc49e1d051f636fd701c
tyloneitor <rodri329@go.stockton.edu>
2023-03-02 20:04:20 -0500

    Final Fix

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,23 @@
 // Assignment Code
-var generateBtn = document.getElementById("generate");
-var length = 8-128;
-var charset = ["a","b","c","d","e","f","g","h","i","j","k","m","n","o","p","q","r","s","t","u","v","w","","x","y","z"]
-var charset = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","","X","Y","Z"]
-var charset = ["0","1","2","4","5","6","7","8","9"]
-//for (var i = 0, i <= length; i++){ 
-
-//var ranchar = math.random(length);
-//writePassword = charset(randonNumber, randonNumber +1);
-   retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
+var generateBtn = document.querySelector("#generate");
 
 
-
-
-// Write password to the #password input// add starter code 
+// Write password to the #password input//
 function writePassword() {
-    document.getElementById("").innerHTML;
-  var password =generatePassword(); 
+  var password = '';
+  var lengthmin = 8;
+  var lengthmax = 120;
+  var length = Math.floor(Math.random() * (lengthmax-lengthmin +1)) + lengthmin; 
+  var Charset = ["a","b","c","d","e","f","g","h","i","j","k","m","n","o","p","q","r","s","t","u","v","w","","x","y","z"];
+   Charset = Charset.concat(["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"])
+   Charset = Charset.concat(["0","1","2","4","5","6","7","8","9"])
+   Charset = Charset.concat(["~","!","@","#","$","%","^","&","*","(",")","_","+"])
+  for ( var i = 0; i < length; i++ ) {
+    password += Charset[Math.floor(Math.random() * Charset.length)];
+  }
   var passwordText = document.querySelector("#password");
 
-passwordText.value = password;
+  passwordText.value = password; 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generate.addEventListener("click", writePassword);
